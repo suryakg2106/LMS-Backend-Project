457a79383de288ac7f621e75f755408a76bffcf6
suryakg2106 <suryakg2106@gmail.com>
2026-02-21 20:53:10 +0530

    Resolve merge conflict

--- Router/AuthRouter.js
+++ Router/AuthRouter.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { Register , Login } from "../Controller/AuthController.js";
 import { body } from "express-validator";
-import  authMiddleware from "../Middleware/RoleMiddleware.js";
+import  {Protected} from "../Middleware/AuthProtected.js";
 
 
 const Router = express.Router();
@@ -19,12 +19,13 @@ Router.post("/login",[
 
 
 // routes/auth.js
-Router.get("/me", authMiddleware, (req, res) => {
+Router.get("/me",Protected,(req, res) => {
   res.status(200).json({
     user: {
       id: req.user.id,
       name: req.user.name,
       email: req.user.email,
+      role: req.user.role,
     },
   });
 });
