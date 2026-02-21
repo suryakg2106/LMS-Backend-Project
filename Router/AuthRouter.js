9e1b7e0cd84bf8bea5921e4aa86f476cf5da08d0
suryakg2106 <suryakg2106@gmail.com>
2026-02-21 20:18:56 +0530

    Add cookie-based auth context with frontend role handling

--- Router/AuthRouter.js
+++ Router/AuthRouter.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { Register , Login } from "../Controller/AuthController.js";
 import { body } from "express-validator";
+import  authMiddleware from "../Middleware/RoleMiddleware.js";
 
 
 const Router = express.Router();
@@ -15,5 +16,17 @@ Router.post("/login",[
     body('email').notEmpty().withMessage('email is required'),
     body('password').notEmpty().withMessage('password is required'),
 ],Login);
+
+
+// routes/auth.js
+router.get("/me", authMiddleware, (req, res) => {
+  res.status(200).json({
+    user: {
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    },
+  });
+});
 
 export default Router;
